317682f0333c2fc2d4c67f3a1a14ff2eb848696d
weswes2EPYC <whu327@gatech.edu>
2024-09-06 20:56:45 -0400

    midway finished SQLite db

--- food_finder/food_finder_app/models.py
+++ food_finder/food_finder_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Restaurant(models.Model):
+    API_id = models.CharField(max_length=255)
