fd88c4dd2a66b90ae905ea6bd463e314809d8668
weswes2EPYC <whu327@gatech.edu>
2024-09-06 22:26:03 -0400

    changed API_id to a unique key in Restaurant table and added __str__ functions in both tables. Fully migrated

--- food_finder/food_finder_app/models.py
+++ food_finder/food_finder_app/models.py
@@ -3,8 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Restaurant(models.Model):
-    API_id = models.CharField(max_length=255)
+    API_id = models.CharField(max_length=255, unique = True)
+
+    def __str__(self):
+        return self.API_id
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     favorite_restaurants = models.ManyToManyField(Restaurant, related_name='favorited_by')
+
+    def __str__(self):
+        return self.name
