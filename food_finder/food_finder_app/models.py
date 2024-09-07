d2f819563eac277efb0ddcd05c6aa7b78f4c7f27
weswes2EPYC <whu327@gatech.edu>
2024-09-06 22:07:16 -0400

    added basic db models for favorite restaurants

--- food_finder/food_finder_app/models.py
+++ food_finder/food_finder_app/models.py
@@ -4,3 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Restaurant(models.Model):
     API_id = models.CharField(max_length=255)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_restaurants = models.ManyToManyField(Restaurant, related_name='favorited_by')
