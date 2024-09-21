db3cd8242761271ec4115120733fb2f4fe43c179
weswes2EPYC <whu327@gatech.edu>
2024-09-21 19:11:47 -0400

    added a search filter so only restaurants show up

--- food_finder/main/views.py
+++ food_finder/main/views.py
@@ -2,45 +2,44 @@ from django.shortcuts import render
 import requests
 from django.conf import settings
 
-# Create your views here.
 def returnHomePage(request):
     return render(request, 'main/home.html', {'isLoggedIn': False})
 
 def restaurants_view(request):
     query = request.GET.get('query', '')
-    
-    # If no query is provided, return an empty result
+    min_rating = request.GET.get('rating', 0)
+
     if not query:
-        return render(request, 'main/restaurants.html', {'restaurants': [], 'query': query})
-    
-    # Make the API request to Google Places
+        return render(request, 'main/restaurants.html', {'restaurants': [], 'query': query, 'selected_rating': min_rating})
+
     url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
     params = {
         'query': query,
+        'type': 'restaurant',
         'key': settings.GOOGLE_PLACES_API_KEY
     }
     
     response = requests.get(url, params=params)
     data = response.json()
-    
-    # Extract relevant information from the API response
+
     restaurants = []
     if 'results' in data:
         for result in data['results']:
-            # Construct the photo URL using the photo_reference
-            photo_reference = result.get('photos', [{}])[0].get('photo_reference', '')
-            if photo_reference:
-                image_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&photoreference={photo_reference}&key={settings.GOOGLE_PLACES_API_KEY}"
-            else:
-                image_url = '/path/to/default/image.jpg'  # Fallback image if no photo is available
+            rating = result.get('rating', 0)
+            if float(rating) >= float(min_rating):
+                photo_reference = result.get('photos', [{}])[0].get('photo_reference', '')
+                if photo_reference:
+                    image_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&photoreference={photo_reference}&key={settings.GOOGLE_PLACES_API_KEY}"
+                else:
+                    image_url = '/path/to/default/image.jpg'
 
-            restaurant = {
-                'image_url': image_url,
-                'name': result.get('name', 'Unknown'),
-                'rating': result.get('rating', 'N/A'),
-                'reviews': result.get('user_ratings_total', 'N/A'),
-                'place_id': result.get('place_id', '')
-            }
-            restaurants.append(restaurant)
+                restaurant = {
+                    'image_url': image_url,
+                    'name': result.get('name', 'Unknown'),
+                    'rating': rating,
+                    'reviews': result.get('user_ratings_total', 'N/A'),
+                    'place_id': result.get('place_id', '')
+                }
+                restaurants.append(restaurant)
 
-    return render(request, 'main/restaurants.html', {'restaurants': restaurants, 'query': query})
+    return render(request, 'main/restaurants.html', {'restaurants': restaurants, 'query': query, 'selected_rating': min_rating})
