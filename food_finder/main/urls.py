e697bd69f9a487cae4ec1498760dc01d54972e5d
jaiden-lee <jaidenwlee@gmail.com>
2024-09-21 22:51:21 -0400

    added favorite function

--- food_finder/main/urls.py
+++ food_finder/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import returnHomePage, restaurantDetailsPage
+from .views import returnHomePage, restaurantDetailsPage, saveRestaurant
 
 urlpatterns = [
     path('', returnHomePage, name='home'),
-    path('restaurant/<int:restaurant_id>/', restaurantDetailsPage, name='details')
+    path('restaurant/<int:restaurant_id>/', restaurantDetailsPage, name='details'),
+    path('save/<str:restaurant_id>/', saveRestaurant, name='save')
     # ... other url patterns
 ]
