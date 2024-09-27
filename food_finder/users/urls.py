7ff8e4c23b5499581440fcc332bc9f2e8270c7e1
weswes2EPYC <whu327@gatech.edu>
2024-09-27 17:52:22 -0400

    delete for merge from restart

--- food_finder/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import signUpUser, signInUser, logoutUser
-
-urlpatterns = [
-    path('signup/', signUpUser, name='signup'),
-    path('signin/', signInUser, name='signin'),
-    path('logout/', logoutUser, name='logout'),
-    # ... other url patterns
-]
