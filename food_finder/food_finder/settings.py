a2343aada196d3ab612bfe2b3e873a5351829b3a
weswes2EPYC <whu327@gatech.edu>
2024-09-28 15:55:27 -0400

    pythonanywhere settings changed

--- food_finder/food_finder/settings.py
+++ food_finder/food_finder/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # Location where Vercel will collect static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
