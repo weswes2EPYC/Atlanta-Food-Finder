abceea58dd4124b196c9dcd3b12358df6007a047
weswes2EPYC <whu327@gatech.edu>
2024-09-27 21:12:24 -0400

    added test url

--- food_finder/food_finder/urls.py
+++ food_finder/food_finder/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),  # Prefix for user-related routes
     path('', include('main.urls')),  # Main app routes start from the root
 ]
+
+def catch_all_view(request, path):
+    return HttpResponse(f"Path not found: {path}", status=404)
+
+urlpatterns += [path('<path:path>', catch_all_view)]
