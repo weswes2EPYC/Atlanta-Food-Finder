6495ce327850913a185b803e7248b5f058dcc643
weswes2EPYC <whu327@gatech.edu>
2024-09-27 20:45:03 -0400

    added static files for vercel

--- food_finder/food_finder/settings.py
+++ food_finder/food_finder/settings.py
@@ -121,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# Location where Vercel will collect static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional: You can specify directories containing your static files (if any)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Default primary key field type
